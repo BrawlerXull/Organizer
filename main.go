b6d3263d5e7c8a805f7f2166fec7a71c7c7f81bf
Chinmay <chaudhari.chinmay12345@gmail.com>
2023-08-02 19:26:45 +0530

    logger added

--- main.go
+++ main.go
@@ -7,25 +7,36 @@ import (
 	"path/filepath"
 	"strings"
 
+	"github.com/charmbracelet/log"
 	"github.com/logrusorgru/aurora/v4"
 )
 
+func setupLogger() *os.File {
+	logFile, err := os.OpenFile("logfile.log", os.O_CREATE|os.O_APPEND|os.O_WRONLY, 0644)
+	if err != nil {
+		log.Fatal("Unable to open log file:", err)
+	}
+	log.SetOutput(logFile)
+	return logFile
+}
+
+func closeLogger(logFile *os.File) {
+	if err := logFile.Close(); err != nil {
+		log.Fatal("Error closing log file:", err)
+	}
+}
+
 func main() {
+	logFile := setupLogger()
+	defer closeLogger(logFile)
 	fmt.Println(aurora.Magenta("Welcome to Ogranizer"))
 	fmt.Println(aurora.Cyan("Enter the directory to clean"))
-
-	// currentUser, err := user.Current()
-	// if err != nil {
-	// 	panic(err)
-	// }
 
 	in := bufio.NewReader(os.Stdin)
 
 	filePathDir, err := in.ReadString('\n')
 	filePathDir = strings.TrimSuffix(filePathDir, "\n")
 	filePathDir = strings.TrimSuffix(filePathDir, "\r")
-
-	// filePathDir := filepath.Join(currentUser.HomeDir, "Desktop/kk")
 
 	files, err := os.ReadDir(filePathDir)
 	if err != nil {
@@ -37,7 +48,6 @@ func main() {
 		fmt.Println(aurora.Yellow(file.Name()))
 		extensionName := filepath.Ext(file.Name())
 		extensionName = strings.TrimPrefix(extensionName, ".")
-		// fmt.Println(extensionName)
 
 		if file.Name() == ".DS_Store" || extensionName == "" {
 			continue
@@ -46,6 +56,7 @@ func main() {
 		err := os.MkdirAll(destinationDir, 0755)
 		if err != nil {
 			fmt.Println("Error creating destination directory:", err)
+			log.Fatal("Error creating destination directory:", err)
 			return
 		}
 
@@ -54,9 +65,11 @@ func main() {
 		err = os.Rename(sourceFilePath, destinationFilePath)
 		if err != nil {
 			fmt.Println("Error moving the file:", err)
+			log.Fatal("Error moving the file:", err)
 			return
 		}
 	}
+	log.Info(filePathDir)
 
 	fmt.Println(aurora.Magenta("Press Enter key to close"))
 	Close, err := in.ReadString('\n')
