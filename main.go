3316537ba527c72bbc387603ac13264680f3a728
Chinmay <chaudhari.chinmay12345@gmail.com>
2023-08-07 23:27:58 +0530

    added cleanOld feature to delete 1 day old dir

--- main.go
+++ main.go
@@ -6,6 +6,7 @@ import (
 	"os"
 	"path/filepath"
 	"strings"
+	"time"
 
 	"github.com/charmbracelet/lipgloss"
 	"github.com/charmbracelet/log"
@@ -49,6 +50,11 @@ func checkerr(err error, Stdlog *log.Logger) {
 	}
 }
 
+func customError(err string, Stdlog *log.Logger) {
+	Stdlog.Error(err)
+	log.Fatal(err)
+}
+
 var welcomeStyle = lipgloss.NewStyle().
 	Bold(true).
 	Foreground(lipgloss.Color("#81efc5")).
@@ -76,43 +82,89 @@ var BorderNotif = lipgloss.NewStyle().
 var infoStyle = lipgloss.NewStyle().
 	Foreground(lipgloss.Color("#f19da5"))
 
+var questionStyle = lipgloss.NewStyle().
+	Bold(true).
+	Foreground(lipgloss.Color("#5477a8")).
+	Padding(0, 1, 0, 1).
+	BorderBottom(true).
+	BorderTop(true)
+
 func main() {
+
+	in := bufio.NewReader(os.Stdin)
+
 	Stdlog := out()
 	logFile := setupLogger()
 	defer closeLogger(logFile)
 	fmt.Println(welcomeStyle.Render("Welcome to Organizer !!"))
-	fmt.Println(InputPromptStyle.Render("Enter the directory to clean"))
+
+	fmt.Println(questionStyle.Render("What do you want to perform ? (cleanOld / organize)"))
 	fmt.Print(arrStyle.Render("> "))
-
-	in := bufio.NewReader(os.Stdin)
-
-	filePathDir, err := in.ReadString('\n')
+	userResp, err := in.ReadString('\n')
 	checkerr(err, Stdlog)
-	filePathDir = strings.TrimSuffix(filePathDir, "\n")
-	filePathDir = strings.TrimSuffix(filePathDir, "\r")
+	userResp = strings.TrimSuffix(userResp, "\n")
+	userResp = strings.TrimSuffix(userResp, "\r")
 
-	files, err := os.ReadDir(filePathDir)
-	checkerr(err, Stdlog)
-	fmt.Println(BorderNotif.Render("Below files are found"))
+	if userResp == "organize" {
+		fmt.Println(InputPromptStyle.Render("Enter the directory to clean"))
+		fmt.Print(arrStyle.Render("> "))
 
-	for _, file := range files {
-		fmt.Println(infoStyle.Render(fmt.Sprintf("* %s", file.Name())))
-		extensionName := filepath.Ext(file.Name())
-		extensionName = strings.TrimPrefix(extensionName, ".")
+		filePathDir, err := in.ReadString('\n')
+		checkerr(err, Stdlog)
+		filePathDir = strings.TrimSuffix(filePathDir, "\n")
+		filePathDir = strings.TrimSuffix(filePathDir, "\r")
 
-		if file.Name() == ".DS_Store" || extensionName == "" {
-			continue
+		files, err := os.ReadDir(filePathDir)
+		checkerr(err, Stdlog)
+		fmt.Println(BorderNotif.Render("Below files are found"))
+
+		for _, file := range files {
+			fmt.Println(infoStyle.Render(fmt.Sprintf("* %s", file.Name())))
+			extensionName := filepath.Ext(file.Name())
+			extensionName = strings.TrimPrefix(extensionName, ".")
+
+			if file.Name() == ".DS_Store" || extensionName == "" {
+				continue
+			}
+			destinationDir := filepath.Join(filePathDir, extensionName)
+			err := os.MkdirAll(destinationDir, 0755)
+			checkerr(err, Stdlog)
+
+			sourceFilePath := filepath.Join(filePathDir, file.Name())
+			destinationFilePath := filepath.Join(destinationDir, file.Name())
+			err = os.Rename(sourceFilePath, destinationFilePath)
+			checkerr(err, Stdlog)
+
+			log.Info(filePathDir)
 		}
-		destinationDir := filepath.Join(filePathDir, extensionName)
-		err := os.MkdirAll(destinationDir, 0755)
-		checkerr(err, Stdlog)
+	} else if userResp == "cleanOld" {
+		fmt.Println(InputPromptStyle.Render("Enter the directory to clean"))
+		fmt.Print(arrStyle.Render("> "))
 
-		sourceFilePath := filepath.Join(filePathDir, file.Name())
-		destinationFilePath := filepath.Join(destinationDir, file.Name())
-		err = os.Rename(sourceFilePath, destinationFilePath)
+		filePathDir, err := in.ReadString('\n')
 		checkerr(err, Stdlog)
+		filePathDir = strings.TrimSuffix(filePathDir, "\n")
+		filePathDir = strings.TrimSuffix(filePathDir, "\r")
+
+		files, err := os.ReadDir(filePathDir)
+		checkerr(err, Stdlog)
+		fmt.Println(BorderNotif.Render("Below files are found"))
+
+		for _, file := range files {
+			fmt.Println(infoStyle.Render(fmt.Sprintf("* %s", file.Name())))
+			cleanFileDir := filePathDir + "/" + file.Name()
+			fileInfo, err := os.Stat(cleanFileDir)
+			checkerr(err, Stdlog)
+			onDayAgo := time.Now().AddDate(0, 0, -1)
+			if fileInfo.ModTime().Before(onDayAgo) {
+				err := os.RemoveAll(cleanFileDir)
+				checkerr(err, Stdlog)
+			}
+			log.Info(filePathDir)
+		}
+	} else {
+		customError(infoStyle.Render("Command not found"), Stdlog)
 	}
-	log.Info(filePathDir)
 
 	fmt.Println(InputPromptStyle.Render("Press Enter key to close"))
 	_, err = in.ReadString('\n')
